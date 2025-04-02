e0cbb80a1b0ef587120b2abc1ba0756f41c037c7
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2025-04-02 20:01:43 +0300

    style: buttons in hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,4 +1,13 @@
 <template>
+  <div class="hero">
+    <div class="hero__buttons">
+      <div class="hero__button" v-for="button in buttons" :key="button.text || button.src">
+        <p v-if="button.text">{{ button.text }}</p>
+        <img v-if="button.src" :src="button.src" :alt="button.alt || 'icon'" />
+      </div>
+    </div>
+    <HeroCarousel />
+  </div>
   <section class="our-rules">
     <article class="our-rules__item" v-for="advantage in advantages" :key="advantage.text">
       <img :src="advantage.icon" class="our-rules__img" />
@@ -14,8 +23,17 @@ import replaceMeal from "@/assets/icons/replace-meal.svg";
 import cookTime from "@/assets/icons/cook-time.svg";
 import delivery from "@/assets/icons/delivery.svg";
 import saveEnergy from "@/assets/icons/save-energy.svg";
+import fish from "@/assets/icons/fish.svg";
+import carrot from "@/assets/icons/carrot.png";
+import bottle from "@/assets/icons/bottle.svg";
+import meat from "@/assets/icons/meat.svg";
+import table from "@/assets/icons/table.png"
+import HeroCarousel from "./HeroCarousel.vue";
 
 export default {
+  components: {
+    HeroCarousel
+  },
   data() {
     return {
       advantages: [
@@ -45,6 +63,14 @@ export default {
           isImage: true,
         },
       ],
+      buttons: [
+        { text: "kcal"},
+        { src: fish, alt: "fish"},
+        { src: carrot, alt: "carrot" },
+        { src: bottle, alt: "bottle" },
+        { src: meat, alt: "meat" },
+        { src: table, alt: "table", text:"5" },
+      ]
     };
   },
 };
@@ -52,6 +78,41 @@ export default {
 
 <style lang="scss" >
 @use "@/assets/styles/_mixins.scss" as *;
+.hero{
+  width: 100%;
+  min-height: calc(100vh - 217px );
+
+  &__buttons{
+    position: absolute;
+    left: 50px;
+    top: 50%;
+    transform: translateY(-50%);
+    @include flexbox($display: flex, $direction: column, $justifyContent: space-between, $alignItems: center);
+    gap: 50px;
+    @include text-style(
+      $font-family: Comfortaa,
+        $weight: 700,
+        $size: 20px,
+        $line-height: 138%,
+        $letter-spacing: 0%,
+        $color: var(--black)
+    )
+  }
+  &__button{
+    width: 70px;
+    height: 70px;
+    @include flexbox($display: flex, $direction: column, $justifyContent: center, $alignItems: center);
+    background: var(--background-block);
+    box-shadow: var(--shadow);
+    border-radius: 50%;
+
+    img{
+      width: 40px;
+      height: 40px;
+      object-fit: contain;
+    }
+  }
+}
 .our-rules {
   max-width: 1286px;
   @include flexbox($display: flex, $wrap: wrap, $justifyContent: space-between, $alignItems: center);
